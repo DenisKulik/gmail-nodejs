92a5dfcf68fcf24351aa142e928472473da222e3
Denis Kulik <kulikdenis1996x@gmail.com>
2023-09-02 12:53:46 +0700

    feat: sending message to gmail

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const nodemailer = require('nodemailer')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
 const app = express()
+
+const corsOptions = {
+    origin: '*',
+    methods: 'POST',
+}
+app.use(cors(corsOptions))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 const transporter = nodemailer.createTransport({
     host: 'smtp.mail.ru',
@@ -16,15 +26,29 @@ const transporter = nodemailer.createTransport({
     },
 })
 
-app.get('/sendMessage', async (req, res) => {
-    const info = await transporter.sendMail({
+app.post('/sendMessage', async (req, res) => {
+    const { name, email, message } = req.body
+
+    const htmlMessage = `
+    	<html>
+    		<head></head>
+    		<body>
+        	<h2>FEEDBACK FORM</h2>
+        	<p><strong>Name:</strong> ${name}</p>
+        	<p><strong>Email:</strong> ${email}</p>
+        	<p><strong>Message:</strong> ${message}</p>
+    		</body>
+    </html>
+		`
+
+    await transporter.sendMail({
         from: 'Portfolio <kdp-work@mail.ru>',
         to: 'kulikdenis1996x@gmail.com',
         subject: 'FEEDBACK FORM',
-        text: 'Test',
+        html: htmlMessage,
     })
 
-    res.send('Hello World!')
+    res.send('ok')
 })
 
 app.listen(3000, () => {
